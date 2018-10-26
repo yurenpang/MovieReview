a5e26673892223f989bb6fe2334496687affec84
yurenpang <ypang@macalester.edu>
2018-10-26 16:48:25 -0500

    Add image

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\ypang\Desktop\hw4\venv"
+$env:VIRTUAL_ENV="C:\Users\ypang\Desktop\hw4_YurenPang\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\ypang\Desktop\hw4\venv"
+set "VIRTUAL_ENV=C:\Users\ypang\Desktop\hw4_YurenPang\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
